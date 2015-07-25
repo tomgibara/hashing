a6aeb79a3f1b2f692f35e3962c0cae0ea8b8de63
Tom Gibara <me@tomgibara.com>
2015-07-25 18:29:37 +0100

    Adds basic tests for java hashing.

---
+++ src/test/java/com/tomgibara/hashing/JavaHasherTest.java
@@ -0,0 +1,33 @@
+package com.tomgibara.hashing;
+
+public class JavaHasherTest extends HashingTest {
+
+	public void testHashCodeCorrect() {
+		Hasher<String> hasher = Hashing.objectHasher();
+		HashSize size = hasher.getSize();
+		assertEquals(HashSize.INT_SIZE, size);
+		for (int i = 100000; i < 110000; i += 10) {
+			String str = Integer.toString(i);
+			assertEquals(str.hashCode(), hasher.intHashValue(str));
+			testConsistent(hasher, str);
+			testCorrectlySizedInts(hasher.hashValue(str), size, 1);
+			testCorrectlySizedLongs(hasher.hashValue(str), size, 1);
+			testCorrectlySizedBigs(hasher.hashValue(str), size, 1);
+		}
+	}
+	
+	public void testIndentityCorrect() {
+		Hasher<String> hasher = Hashing.identityHasher();
+		HashSize size = hasher.getSize();
+		assertEquals(HashSize.INT_SIZE, size);
+		for (int i = 100000; i < 110000; i += 10) {
+			String str = Integer.toString(i);
+			assertEquals(System.identityHashCode(str), hasher.intHashValue(str));
+			testConsistent(hasher, str);
+			testCorrectlySizedInts(hasher.hashValue(str), size, 1);
+			testCorrectlySizedLongs(hasher.hashValue(str), size, 1);
+			testCorrectlySizedBigs(hasher.hashValue(str), size, 1);
+		}
+	}
+	
+}
