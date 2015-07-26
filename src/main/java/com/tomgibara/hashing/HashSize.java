faa2d9c53753056f32917f77da7a5ecd1dc669b5
Tom Gibara <me@tomgibara.com>
2015-07-26 14:34:35 +0100

    Documents HashSize.

--- src/main/java/com/tomgibara/hashing/HashSize.java
+++ src/main/java/com/tomgibara/hashing/HashSize.java
@@ -34,13 +34,32 @@ public final class HashSize implements Comparable<HashSize> {
 	final static BigInteger BIG_UINT = BigInteger.valueOf(LONG_UINT);
 	final static BigInteger BIG_ULONG = BigInteger.ONE.shiftLeft(64);
 
-	// has byte capacity
+	/**
+	 * A hash size with maximum byte capacity; hash values may span all 2^8
+	 * possible byte values.
+	 */
+
 	public static final HashSize BYTE_SIZE = new HashSize(BIG_BYTE);
-	// has short capacity
+
+	/**
+	 * A hash size with maximum short capacity; hash values may span all 2^16
+	 * possible short values.
+	 */
+
 	public static final HashSize SHORT_SIZE = new HashSize(BIG_SHORT);
-	// has int capacity
+
+	/**
+	 * A hash size with maximum int capacity; hash values may span all 2^32
+	 * possible int values.
+	 */
+
 	public static final HashSize INT_SIZE = new HashSize(BIG_UINT);
-	// has long capacity
+
+	/**
+	 * A hash size with maximum long capacity; hash values may span all 2^64
+	 * possible long values.
+	 */
+
 	public static final HashSize LONG_SIZE = new HashSize(BIG_ULONG);
 
 	// to call forXXX?
@@ -154,38 +173,100 @@ public final class HashSize implements Comparable<HashSize> {
 
 	// accessors
 
+	/**
+	 * The size as a big integer.
+	 * 
+	 * @return the size, always greater than zero
+	 */
+
 	public BigInteger asBig() {
 		return bigSize;
 	}
+
+	/**
+	 * The number of bits required to store hashes of this size.
+	 * 
+	 * @return the number of bits, always greater than zero
+	 */
 
 	public int getBits() {
 		return bits;
 	}
 
+	/**
+	 * Whether hash values of this size are composed of an exact number of bits.
+	 * 
+	 * @return whether the size is a power of two
+	 */
+	
 	public boolean isPowerOfTwo() {
 		return powerOfTwo;
 	}
+
+	/**
+	 * Whether this size is small enough to be represented by a signed int.
+	 * 
+	 * @return whether the hash size is int-sized
+	 */
 
 	public boolean isIntSized() {
 		return intSized;
 	}
 
+	/**
+	 * Whether hashes of this size can be accommodated in the bits of a single
+	 * int.
+	 * 
+	 * @return whether an int has the capacity to store hashes of this size
+	 */
+
 	public boolean isIntCapacity() {
 		return intCapacity;
 	}
 
-	public int asInt() {
+	/**
+	 * The size as an int.
+	 * 
+	 * @return the size, always greater than zero
+	 * @throws ArithmeticException
+	 *             if not int-sized
+	 * @see #intSized
+	 */
+
+	public int asInt() throws ArithmeticException {
 		if (!intSized) throw new ArithmeticException("not int sized");
 		return intSize;
 	}
+
+	/**
+	 * Whether this size is small enough to be represented by a signed long.
+	 * 
+	 * @return whether the hash size is long-sized
+	 */
 
 	public boolean isLongSized() {
 		return longSized;
 	}
 
+	/**
+	 * Whether hashes of this size can be accommodated in the bits of a single
+	 * long.
+	 * 
+	 * @return whether a long has the capacity to store hashes of this size
+	 */
+
 	public boolean isLongCapacity() {
 		return longCapacity;
 	}
+
+	/**
+	 * The size as a long.
+	 * 
+	 * @return the size, always greater than zero
+	 * @throws ArithmeticException
+	 *             if not long-sized
+	 * @see #longSized
+	 */
 
 	public long asLong() {
 		if (!longSized) throw new ArithmeticException("not long sized");
@@ -194,6 +275,19 @@ public final class HashSize implements Comparable<HashSize> {
 
 	// methods
 
+	/**
+	 * Maps an arbitrary integer into the range of this size by computing the
+	 * remainder of the int modulo this size. For sizes that are a power of two,
+	 * this is equivalent to returning the number of least significant bits
+	 * indicated by {@link #getBits()}. Note that for sizes that are not
+	 * int-sized (ie. sizes exceeding 2^31-1) the resulting value may be
+	 * negative.
+	 * 
+	 * @param value
+	 *            the value to be mapped into the range of this size.
+	 * @return an int whose unsigned value falls into the range of this size
+	 */
+
 	public int mapInt(int value) {
 		if (!intCapacity) return value;
 		if (powerOfTwo) return value & intMask;
@@ -201,12 +295,36 @@ public final class HashSize implements Comparable<HashSize> {
 		return (int) ((LONG_UINT - value) % longSize);
 	}
 
+	/**
+	 * Maps an arbitrary long into the range of this size by computing the
+	 * remainder of the long modulo this size. For sizes that are a power of
+	 * two, this is equivalent to returning the number of least significant bits
+	 * indicated by {@link #getBits()}. Note that for sizes that are not
+	 * long-sized (ie. sizes exceeding 2^63-1) the resulting value may be
+	 * negative.
+	 * 
+	 * @param value
+	 *            the value to be mapped into the range of this size.
+	 * @return a long whose unsigned value falls into the range of this size
+	 */
+
 	public long mapLong(long value) {
 		if (!longCapacity) return value;
 		if (powerOfTwo) return value & longMask;
 		if (longSized & value >= 0L) return value % longSize;
 		return BigInteger.valueOf(value).mod(bigSize).longValue();
 	}
+
+	/**
+	 * Maps an arbitrary big integer into the range of this size by computing
+	 * its remainder modulo this size. For sizes that are a power of two, this
+	 * is equivalent to returning the number of least significant bits indicated
+	 * by {@link #getBits()}.
+	 * 
+	 * @param value
+	 *            the value to be mapped into the range of this size.
+	 * @return a big integer less than this size
+	 */
 
 	public BigInteger mapBig(BigInteger value) {
 		return powerOfTwo ? value.and(mask) : value.mod(bigSize);
